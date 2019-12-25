a61d7f59419967ba1109b54920187088c635a450
LeoFernanndes <58197167+LeoFernanndes@users.noreply.github.com>
2019-12-25 02:15:32 -0300

    Update forward_selection.py

--- forward_selection.py
+++ forward_selection.py
@@ -65,3 +65,49 @@ def forward_selection(df):
         'Used feadtures:', '\n',
         df_sorted.columns[0:n], '\n',
         'Score: ', score.mean())
+
+
+
+  def forward_selection_regression(df):  
+
+  used_features = []
+  score = 1
+
+  for i in range(len(df.columns) - 1):
+
+    n = i + 1
+    '''
+    if i == 0:
+      score = 1
+    else:
+      erro_opt_it
+    '''
+    df_sorted = df[list(chi2_df['feature'])]
+    array_sorted = df_sorted.values
+
+    used_features = []
+
+    X_opt = array_sorted[:, 0: n]
+    Y_opt = Y
+
+    reg_opt_it = RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
+            max_features='auto', max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=1,
+            oob_score=False, random_state=None, verbose=0, warm_start=False)
+    
+    
+    
+    reg_opt_it.fit(x_opt_train, y_opt_train)
+    y_opt_pred_it = reg_opt_it.predict(x_opt_test)
+
+    erro_opt_it = np.sqrt(mean_squared_log_error(y_opt_test, y_opt_pred_it))
+    erro_opt_it
+
+
+    if erro_opt_it < score:
+      score = erro_opt_it
+      used_features.append(df_sorted.columns[i])
+
+  return [len(used_features), score]
